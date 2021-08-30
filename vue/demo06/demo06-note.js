9b7dab3efdf285cd412ffd3475c47c843808ae29
fengandzhy <fengandzhy@gmail.com>
2021-08-31 04:07:26 +1200

    demo06

--- vue/demo06/demo06-note.js
+++ vue/demo06/demo06-note.js
@@ -3,6 +3,10 @@
  *
  *  2. @click="greet" 是它的缩写
  *
- *  3. 它会自带一个参数e, 它是$event事件参数, 可以通过e.target.value取到值 
+ *  3. 它会自带一个参数e, 它是$event事件参数, 可以通过e.target.value取到值
  *
+ *  4. 如果写成@keyup.enter="enter()",那么方法中定义的这个参数e就不起作用了必须写成@keyup.enter="enter($event)" 或者@keyup.enter="enter"
+ *
+ *
+ *  
  * */
