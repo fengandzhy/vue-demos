e0cc8c2a6d6ab2d137ecf92c372180d788059b57
fengandzhy <fengandzhy@gmail.com>
2021-09-25 05:00:30 +1200

    some comments

--- vue/demo08/demo08-note.js
+++ vue/demo08/demo08-note.js
@@ -4,5 +4,7 @@
  * 2. 对于{{msg}}来说, 它首先会去找data()里面的msg变量, 如果这里没有, 它会去找computed里面的msg方法的返回值
  *
  * 3. 另外对于{{msg}}来说, 它是可以找到computed里面的msg方法, 但是v-model="msg"却只能绑定到data()里面, 无法绑定到computed
+ *
+ *
  *  
  * */
--- vue/demo10/demo10-note.js
+++ vue/demo10/demo10-note.js
@@ -6,5 +6,11 @@
  *
  * 3. 如果 {{member}} 这里的member是computed里面的一个函数, 此函数必须要有return 才能让前台页面看到值, 否则看不到值.
  *
+ * 4. v-model="member" 要在computed里面起作用, 第一这个computed 里面的member必须是变量. 第二这个变量必须是set get方法, 而且这个
+ * set get方法最终落实到data()里面的变量. 假设在computed里面还有一个member1, 你把这个member的set get方法落实到member1上,但是这个
+ * member1也是要有set get 方法的, 在member1的set get方法上最终还是要落实到data()里面的的变量上. 
+ *
+ *
+ *
  *
  * */
