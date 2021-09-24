d1b0ecec5f32e1572a840889d3c8f162cf865f81
fengandzhy <fengandzhy@gmail.com>
2021-09-25 04:41:16 +1200

    vue v-bind

--- vue/demo08/demo08-note.js
+++ vue/demo08/demo08-note.js
@@ -1,6 +1,8 @@
 /**
  * 1. v-model 是模型绑定 v-model="msg" 就是把元素的值绑定到data()的变量中去
  *
+ * 2. 对于{{msg}}来说, 它首先会去找data()里面的msg变量, 如果这里没有, 它会去找computed里面的msg方法的返回值
  *
+ * 3. 另外对于{{msg}}来说, 它是可以找到computed里面的msg方法, 但是v-model="msg"却只能绑定到data()里面, 无法绑定到computed
  *  
  * */
