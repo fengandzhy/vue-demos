537eea968f4d66b2ab8ee057d57430f16714699e
fengandzhy <fengandzhy@gmail.com>
2021-09-26 20:37:33 +1300

    vue-xue demo01

---
+++ vue-xu/demo01/demo01-index.js
@@ -0,0 +1,11 @@
+/**
+ * 1. 当引用vue.js的时候<script src="../node_modules/vue/dist/vue.js"></script> 就已经可以 new 一个Vue了
+ *
+ * 2. MVVM的这个模型中 这几个部分分别参考本例的注解
+ * M 指Model是数据
+ * V 指试图是Dom
+ * VM ViewModel是负责建立数据与Dom之间联系的
+ *
+ *
+ *
+ * */
