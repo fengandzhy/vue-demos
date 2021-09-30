e413249e2fabd933a9193893af7aea17b89e86b7
fengandzhy <fengandzhy@gmail.com>
2021-10-01 06:07:12 +1300

    vue-demo09

--- vue/demo03/demo03-note.js
+++ vue/demo03/demo03-note.js
@@ -5,7 +5,7 @@
  *
  * 3. 把data里面的值放到某个属性里面去, 用v-bind:属性, <a v-bind:href="url">百度</a><br/>
  * 这种指令可以缩写<a :href="url">百度</a><br/> 也可以用将属性名赋值给变量<a :[attr_href]="url">百度</a><br/>
- * attr_href 是个变量名,它的值是href, 特别要注意的是这个变量名必须是小写.
+ * attr_href 是个变量名,它的值是href (参看下方), 特别要注意的是这个变量名必须是小写.
  *
  * 4. 大括号里可以运行javascript {{url.toUpperCase()}}
  *
