e777ecb592e11b4758c72077862130a46552b29e
fengandzhy <fengandzhy@gmail.com>
2021-10-03 21:37:08 +1300

    vue-demo23

---
+++ vue/demo23/demo23-note.js
@@ -0,0 +1,10 @@
+/**
+ * 1. 一般情况下 在父组件中调用子组件 <my-button>按钮</my-button> 按钮两个字是显示不出来的, 按钮两个字需要在子组件中设置
+ * 但如果在子组件中加入了<slot></slot> 例如 <button type="button"><slot></slot></button> 那么按钮两个字就可以显示出来了.
+ *
+ * 2. 一般情况下我们不会在子组件中加入名称,我们只能在父组件中加入, 例如这个按钮叫提交 <my-button type="submit">提交</my-button>
+ * 那个按钮叫再来一个 <my-button>再来一个</my-button> 那么就需要在子组件中设置 <slot></slot>
+ *
+ *
+ *
+ * */
