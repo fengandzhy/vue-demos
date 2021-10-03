0be237760556fde6dfe76655046dd5ef4706f9cb
fengandzhy <fengandzhy@gmail.com>
2021-10-03 22:10:04 +1300

    vue-demo25

--- vue/demo25/demo25-note.js
+++ vue/demo25/demo25-note.js
@@ -3,4 +3,8 @@
  *  1) #header="slotProps" 这里的slotProps是一个任意定义的变量
  *  2) 用上面任意定义的变量 {{slotProps.value}}{{slotProps.value1}}{{slotProps.value2}} 取值
  *
+ * 2. 一般情况下用解构赋值的方法来代替上面的取值方式<slot name="footer"  value="A" value1="B" value2="C"></slot>
+ * v-slot:footer="{value,value1,value2:val2}" <h2>我是页脚{{value}},{{value1}},{{val2}}</h2>
+ * 
+ *
  * */
