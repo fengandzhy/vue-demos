409ee60273b484455dee08109453e6098b7060f7
fengandzhy <fengandzhy@gmail.com>
2021-11-06 14:50:58 +1300

    vue-xu demo04

--- vue-xu/demo04/demo04-index.js
+++ vue-xu/demo04/demo04-index.js
@@ -1,5 +1,6 @@
 /**
- * 1. v-bind绑定样式 :style="myStyle" 在这里myStyle里面 原先的font-size: 20px 要写成 fontSize: '20px' 原先的 color: red 要写成 color: 'red'
+ * 1. v-bind绑定样式 :style="myStyle" 在这里myStyle里面 原先的font-size: 20px 要写成 fontSize: '20px'
+ * 原先的 color: red 要写成 color: 'red'
  *
  *
  *
