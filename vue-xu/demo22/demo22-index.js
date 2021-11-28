3a0ae33ff19e7d5ccf0609f41c5aa29e9781adf4
fengandzhy <fengandzhy@gmail.com>
2021-11-28 15:32:21 +1300

    vue-xu demo22

---
+++ vue-xu/demo22/demo22-index.js
@@ -0,0 +1,10 @@
+/**
+ * 1. 通过 Vue 实例属性 vm.$el 即可获取到 Vue 实例挂载到的 dom元素，返回的是一个 dom 对象，如 div，拿到 dom 元素后我们可以为它设置样式之类的。
+ * 例如 vm.$el.style.color="red";
+ *
+ *
+ *2.通过 Vue 实例属性 vm.$data 即可获取到 Vue 实例 data 选项数据对象，返回的是一个对象。
+ *
+ *3. 通过vm.$method无法取到method属性
+ *
+ * */
