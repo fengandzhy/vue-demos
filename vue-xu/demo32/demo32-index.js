5aeea8466305fd424b66783d329936cfa5b7aa1c
fengandzhy <fengandzhy@gmail.com>
2021-12-03 07:44:29 +1300

    vue-xu demo32

---
+++ vue-xu/demo32/demo32-index.js
@@ -0,0 +1,8 @@
+/**
+ * 1. animation的本质就是它会通过animation来定义一个举动,例如本例中bounce-in就是个举动, 然后规定举动的时间和方向, 例如
+ * animation: bounce-in 1s; animation: bounce-in 3s reverse;
+ *
+ * 2. 然后通过这个来定义这个举动是什么内容
+ *
+ *
+ * */
