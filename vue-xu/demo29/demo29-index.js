1593172030aa4d9972344b10be7b62339b7b5d2d
fengandzhy <fengandzhy@gmail.com>
2021-12-03 07:42:23 +1300

    vue-xu demo31

--- vue-xu/demo29/demo29-index.js
+++ vue-xu/demo29/demo29-index.js
@@ -1,5 +1,10 @@
 /**
- * 1.
+ * 1. xxx-enter ：定义显示前的效果。
+ * 2. xxx-enter-active ：定义显示过程的效果。
+ * 3. xxx-enter-to : 定义显示后的效果。
+ * 4. xxx-leave : 定义隐藏前的效果。
+ * 5. xxx-leave-active ：定义隐藏过程的效果。
+ * 6. xxx-leave-to :定义隐藏后的效果。
  *
  *
  *
